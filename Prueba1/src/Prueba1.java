4d439d51c8a190cd91f45363ef7bd103b969905a
chrq2018 <44592779+chrq2018@users.noreply.github.com>
2022-11-08 10:54:45 -0300

    Update Prueba1.java

--- Prueba1/src/Prueba1.java
+++ Prueba1/src/Prueba1.java
@@ -3,7 +3,7 @@ public class Prueba1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("Hola Mundo");
+		System.out.print("Hola Mundo Java"); 
 	}
 
 }
