82f6f92c951f93be197bfa17e4a1d4aa1f5b6ac3
Christian Quiroga <quirogachristian@gmail.com>
2022-11-08 16:08:33 -0300

    primer ejercio de THP

---
+++ THP/src/tp/TP1_A1_Ej1.java
@@ -0,0 +1,10 @@
+package tp;
+import java.util.Scanner;
+public class TP1_A1_Ej1 {
+	private static Scanner input = new Scanner(System.in);
+	public static void main(String[] args) {
+		System.out.println("Ingrese su nombre ");
+		String nombre = input.nextLine();
+		System.out.println("Bienvenido " + nombre);
+	}
+}
